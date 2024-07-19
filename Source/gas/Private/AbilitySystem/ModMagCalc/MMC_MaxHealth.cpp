7d13df3ac9fa596a3e2601d645cdeaaa2013bac2
Francisco Garciarena <francisco.garciarena94@gmail.com>
2024-07-19 16:27:44 -0300

    reversed files

--- Source/gas/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
+++ Source/gas/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -1,19 +1,18 @@
-
-#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
 
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Interaction/CombatInterface.h"
 
-UMMC_MaxMana::UMMC_MaxMana()
+UMMC_MaxHealth::UMMC_MaxHealth()
 {
-	IntDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
-	IntDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
-	IntDef.bSnapshot = false;
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
 
-	RelevantAttributesToCapture.Add(IntDef);
+	RelevantAttributesToCapture.Add(VigorDef);
 }
 
-float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
 {
 	// Gather tags from source and target
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
@@ -23,12 +22,12 @@ float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectS
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
 
-	float Int = 0.f;
-	GetCapturedAttributeMagnitude(IntDef, Spec, EvaluationParameters, Int);
-	Int = FMath::Max<float>(Int, 0.f);
+	float Vigor = 0.f;
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
+	Vigor = FMath::Max<float>(Vigor, 0.f);
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
 	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
 
-	return 50.f + 2.5f * Int + 15.f * PlayerLevel;
+	return 80.f + 2.5f * Vigor + 10.f * PlayerLevel;
 }
