6b80fb352262a5532cfbd1ddd1d549926b98f7b0
Francisco Garciarena <francisco.garciarena94@gmail.com>
2024-07-22 18:31:48 -0300

    native attributes added and updated widgets controllers

--- Source/gas/Public/UI/HUD/AuraHUD.h
+++ Source/gas/Public/UI/HUD/AuraHUD.h
@@ -4,6 +4,7 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+class UAttributeMenuWidgetController;
 class UAttributeSet;
 class UAbilitySystemComponent;
 class UOverlayWidgetController;
@@ -18,10 +19,8 @@ class GAS_API AAuraHUD : public AHUD
 	GENERATED_BODY()
 public:
 
-	UPROPERTY()
-		TObjectPtr<UAuraUserWidget>  OverlayWidget;
-
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -29,6 +28,9 @@ protected:
 
 
 private:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget>  OverlayWidget;
 
 	UPROPERTY(EditAnywhere)
 		TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
@@ -38,4 +40,10 @@ private:
 
 	UPROPERTY(EditAnywhere)
 		TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+
+	UPROPERTY()
+		TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
 };
